0570e7c193f4768ff229b3167d11ba39285c4a14
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2021-07-21 14:05:57 +0100

    Having a change of mind

--- brainfuck/src/main.rs
+++ brainfuck/src/main.rs
@@ -1,7 +1,8 @@
 use std::io::Read;
 
 fn main() {
-    BFVM::new("+++++++++++++[->++>>>+++++>++>+<<<<<<]>>>>>++++++>--->>>>>>>>>>+++++++++++++++[[
+    BFVM::new(
+        "+++++++++++++[->++>>>+++++>++>+<<<<<<]>>>>>++++++>--->>>>>>>>>>+++++++++++++++[[
         >>>>>>>>>]+[<<<<<<<<<]>>>>>>>>>-]+[>>>>>>>>[-]>]<<<<<<<<<[<<<<<<<<<]>>>>>>>>[-]+
         <<<<<<<+++++[-[->>>>>>>>>+<<<<<<<<<]>>>>>>>>>]>>>>>>>+>>>>>>>>>>>>>>>>>>>>>>>>>>
         >+<<<<<<<<<<<<<<<<<[<<<<<<<<<]>>>[-]+[>>>>>>[>>>>>>>[-]>>]<<<<<<<<<[<<<<<<<<<]>>
@@ -145,95 +146,166 @@ fn main() {
         >>>]>[-]+<]]+>[-<[>>>>>>>>>]<<<<<<<<]>>>>>>>>]<<<<<<<<<[<<<<<<<<<]>>>>[-]<<<++++
         +[-[->>>>>>>>>+<<<<<<<<<]>>>>>>>>>]>>>>>->>>>>>>>>>>>>>>>>>>>>>>>>>>-<<<<<<[<<<<
         <<<<<]]>>>]
-        ").run();
+        ",
+    )
+    .run();
+}
+
+#[derive(Eq, PartialEq)]
+enum Direction {
+    Forward,
+    Backward,
+}
+
+enum OpCode {
+    Increment(u8),
+    Decrement(u8),
+    MoveLeft(usize),
+    MoveRight(usize),
+    Zero,
+    Read,
+    Write,
+    Jump {
+        destination: usize,
+        direction: Direction,
+    },
+}
+
+fn compile(code: &Vec<char>) -> Vec<OpCode> {
+    let mut res = Vec::new();
+    let mut index = 0;
+    let mut jumps: Vec<usize> = Vec::new();
+
+    while index < code.len() {
+        match code[index] {
+            '<' | '>' | '+' | '-' => {
+                let hit = code[index];
+                let mut len = 1;
+                while index + len < code.len() && code[index + len] == hit {
+                    len += 1;
+                }
+
+                let opcode = match code[index] {
+                    '<' => OpCode::MoveLeft(len),
+                    '>' => OpCode::MoveRight(len),
+                    '+' => OpCode::Increment(len as u8),
+                    '-' => OpCode::Decrement(len as u8),
+                    _ => panic!(),
+                };
+
+                res.push(opcode);
+                index += len;
+            }
+            '.' => {
+                res.push(OpCode::Write);
+                index += 1;
+            }
+            ',' => {
+                res.push(OpCode::Read);
+                index += 1;
+            }
+
+            '[' => {
+                if index + 2 < code.len() && code[index + 1] == '-' && code[index + 2] == ']' {
+                    // [-]
+                    res.push(OpCode::Zero);
+                    index += 3;
+                } else {
+                    res.push(OpCode::Jump {
+                        direction: Direction::Forward,
+                        destination: 0,
+                    });
+                    index += 1;
+
+                    jumps.push(res.len() - 1);
+                }
+            }
+            ']' => {
+                let dest = jumps.pop().unwrap();
+
+                if !matches!(&res[dest], OpCode::Jump{destination : _, direction : _}) {
+                    panic!();
+                }
+
+                res[dest] = OpCode::Jump {
+                    direction: Direction::Forward,
+                    destination: res.len(),
+                };
+
+                res.push(OpCode::Jump {
+                    direction: Direction::Backward,
+                    destination: dest,
+                });
+                index += 1;
+            }
+            _ => index += 1,
+        }
+    }
+    res
 }
 
 struct BFVM {
-    memory : [u8; 300000],
-    mem_pointer : usize,
+    memory: [u8; 300000],
+    mem_pointer: usize,
 
-    code : Vec<char>,
-    code_pointer : usize,
+    code: Vec<OpCode>,
+    code_pointer: usize,
 }
 
 impl BFVM {
-    fn new(code : &str) -> Self {
+    fn new(code: &str) -> Self {
         Self {
-            memory : [0; 300000],
-            mem_pointer : 0,
-            code : code.chars().collect(),
-            code_pointer : 0,
+            memory: [0; 300000],
+            mem_pointer: 0,
+            code: compile(&code.chars().collect()),
+            code_pointer: 0,
         }
     }
-    
+
     fn run(&mut self) {
-        loop {
-
-            if self.code_pointer == self.code.len() {
-                return;
-            }
-
-            match self.code[self.code_pointer] {
-                '>' => self.mem_pointer += 1,
-                '<' => self.mem_pointer -= 1,
-                '+' => self.memory[self.mem_pointer] = self.memory[self.mem_pointer].overflowing_add(1).0,
-                '-' => self.memory[self.mem_pointer] = self.memory[self.mem_pointer].overflowing_sub(1).0,
-                '.' => print!("{}", self.memory[self.mem_pointer] as char),
-                ',' => self.read_input(),
+        while self.code_pointer < self.code.len() {
+            match &self.code[self.code_pointer] {
+                OpCode::Increment(i) => {
+                    self.memory[self.mem_pointer] =
+                        self.memory[self.mem_pointer].overflowing_add(i.clone()).0
+                }
+                OpCode::Decrement(i) => {
+                    self.memory[self.mem_pointer] =
+                        self.memory[self.mem_pointer].overflowing_sub(i.clone()).0
+                }
+                OpCode::MoveLeft(i) => self.mem_pointer -= i,
+                OpCode::MoveRight(i) => self.mem_pointer += i,
+                OpCode::Zero => self.memory[self.mem_pointer] = 0,
+                OpCode::Read => self.read_input(),
+                OpCode::Write => print!("{}", self.memory[self.mem_pointer] as char),
                 _ => (),
             }
 
-            match self.code[self.code_pointer] {
-                '[' => self.jump_forward(),
-                ']' => self.jump_backwards(),
+            match &self.code[self.code_pointer] {
+                OpCode::Jump {
+                    destination,
+                    direction,
+                } => {
+                    let zero = self.memory[self.mem_pointer] == 0;
+                    if (direction == &Direction::Forward && zero)
+                        || direction == &Direction::Backward && !zero
+                    {
+                        self.code_pointer = destination.clone();
+                    } else {
+                        self.code_pointer += 1;
+                    }
+                }
                 _ => self.code_pointer += 1,
             }
         }
     }
 
-    fn jump_forward(&mut self) {
-        if self.memory[self.mem_pointer] == 0 {
-            let mut counter = 1;
-
-            while counter != 0 {
-                self.code_pointer += 1;
-
-                match self.code[self.code_pointer] {
-                    '[' => counter += 1,
-                    ']' => counter -= 1,
-                    _ => ()
-                }
-            }
-        } else {
-            self.code_pointer += 1;
-        }
-    }
-
-
-    fn jump_backwards(&mut self) {
-        if self.memory[self.mem_pointer] != 0 {
-            let mut counter = 1;
-
-            while counter != 0 {
-                self.code_pointer -= 1;
-                
-                match self.code[self.code_pointer] {
-                    '[' => counter -= 1,
-                    ']' => counter += 1,
-                    _ => ()
-                }
-            }
-        } else {
-            self.code_pointer += 1;
-        }
-    }
-
     fn read_input(&mut self) {
         let input: Option<u8> = std::io::stdin()
-            .bytes() 
+            .bytes()
             .next()
             .and_then(|result| result.ok());
-        
+
         self.memory[self.mem_pointer] = input.unwrap();
     }
 }
