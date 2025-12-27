5c5c654d0a0d74a0530b689ec9442efca129c123
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2025-12-27 16:00:54 +0000

    Detecting spikes

---
+++ dotnet-trace/fix.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+from typing import List, Dict, Any
+
+def find_discontinuities(trace_events : List[Dict[str, Any]]) -> List[int]:
+    """Finds all sequences of 100 "Begin" trace events that share the same timestamp
+
+    Returns:
+        List[int]: List of indices that point to the start of discontinuities or "spikes" in the list of trace events
+    """
+    res = []
+
+    candidate = None
+    for index, event in enumerate(trace_events):
+        if (event["ph"]) == "E":
+            candidate = None
+
+        elif candidate == None:
+            candidate = (index, event["ts"])
+
+        elif index - candidate[0] == 100:
+            # We found a spike!
+            res.append(candidate[0])
+            candidate = None
+        
+        elif candidate[1] != event["ts"]:
+            # If the timestamp doesn't match, reset the candidate
+            candidate = (index, event["ts"])
+
+    return res
+
+if __name__ == "__main__":
+    with open(Path(__file__).parent / "a.chromium.json") as f: 
+        trace_file : dict = json.load(f)
+    
+    for i in find_discontinuities(trace_file["traceEvents"]):
+        event = trace_file["traceEvents"][i]
+        print(f"Index: {i}, Thread: {event["tid"]}, Timestamp: {round(event["ts"] / 1_000_000, 2)}s, Name: {event["name"]}")
+    
