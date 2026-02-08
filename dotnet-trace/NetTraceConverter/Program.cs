dc310f0d7ee8626c0b9f98b8e894266e43f8cd93
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2026-02-08 18:28:52 +0000

    baby steps

---
+++ dotnet-trace/NetTraceConverter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+
+Option<FileInfo> fileOption = new("--file")
+{
+    Description = "The file to read and display on the console"
+};
+
+RootCommand rootCommand = new("Sample app for System.CommandLine");
+rootCommand.Options.Add(fileOption);
+
+ParseResult parseResult = rootCommand.Parse(args);
+if (parseResult.Errors.Count == 0 && parseResult.GetValue(fileOption) is FileInfo parsedFile)
+{
+    ReadFile(parsedFile);
+    return 0;
+}
+foreach (ParseError parseError in parseResult.Errors)
+{
+    Console.Error.WriteLine(parseError.Message);
+}
+
+static void ReadFile(FileInfo file)
+{
+    foreach (string line in File.ReadLines(file.FullName))
+    {
+        Console.WriteLine(line);
+    }
+}
+
+return 0;
