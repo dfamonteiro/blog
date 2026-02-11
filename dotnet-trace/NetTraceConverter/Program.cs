d2c9fd7618eb6292df6daae9495c7b30516d247c
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2026-02-11 01:21:01 +0000

    Converter MVP

--- dotnet-trace/NetTraceConverter/Program.cs
+++ dotnet-trace/NetTraceConverter/Program.cs
@@ -12,20 +12,7 @@ rootCommand.Options.Add(fileOption);
 ParseResult parseResult = rootCommand.Parse(args);
 if (parseResult.Errors.Count == 0 && parseResult.GetValue(fileOption) is FileInfo parsedFile)
 {
-    ReadFile(parsedFile);
-    return 0;
-}
-foreach (ParseError parseError in parseResult.Errors)
-{
-    Console.Error.WriteLine(parseError.Message);
-}
-
-static void ReadFile(FileInfo file)
-{
-    foreach (string line in File.ReadLines(file.FullName))
-    {
-        Console.WriteLine(line);
-    }
+    TraceFileFormatConverter.ConvertToFormat(Console.Out, Console.Out, TraceFileFormat.Chromium, parsedFile.FullName, "out.chromium.json");
 }
 
 return 0;
