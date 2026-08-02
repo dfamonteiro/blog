323257e22d211bf500df119939f195b7a0cd9112
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2026-08-02 02:30:44 +0100

    cmf*

--- dotnet-trace-final-fix/Program.cs
+++ dotnet-trace-final-fix/Program.cs
@@ -20,7 +20,7 @@ class Program
                 "C:\\Users\\Daniel\\Desktop\\github\\blog\\dotnet-trace-final-fix\\finalboss\\dotnet_20260727_184408.nettrace",
                 "C:\\Users\\Daniel\\Desktop\\github\\blog\\dotnet-trace-final-fix\\finalboss\\test.chromium.json",
                 "Cmf*.Services.*Controller.*",
-                null
+                "Cmf*"
             );
     }
 }
